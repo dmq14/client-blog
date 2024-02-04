699112a7e581456b4ea0c6c6cc2e9db5afe1b86c
dmq14 <dangminhquang1401@gmail.com>
2024-02-04 10:10:53 +0700

    push

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        DEV.from is a clone of{' '}
+        <a href='https://dev.to/' className='hvr-underline'>
+          DEV.to
+        </a>{' '}
+      </p>
+   
+    </footer>
+  );
+};
+
+export default Footer;
